c6495847898fb84292dbbf9da5dfc7819f8ce66d
Carlos Munoz <carlos.munoz.us03@gmail.com>
2022-01-03 14:25:56 -0600

    Added oop package and some class

---
+++ src/oop/Car.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class Car {
+    //attributes
+    private String color;
+    private String brand;
+    private int speed;
+    private int price;
+}
---
+++ src/oop/CarMain.java
@@ -0,0 +1,7 @@
+package oop;
+
+public class CarMain {
+    public static void main(String[] args) {
+
+    }
+}
