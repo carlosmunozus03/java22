930882ebe6c71bec66016da1a8f22ad547f8be10
Carlos Munoz <carlos.munoz.us03@gmail.com>
2022-01-06 16:00:01 -0600

    Bass practice

--- src/oop/instruments/bass/Bass.java
+++ src/oop/instruments/bass/Bass.java
@@ -8,6 +8,17 @@ public class Bass {
     private String coils; //Active or passive
     private int strings; //Number of strings
 
+    //Constructor
+
+
+    public Bass(String brand, String color, String model, String coils, int strings) {
+        this.brand = brand;
+        this.color = color;
+        this.model = model;
+        this.coils = coils;
+        this.strings = strings;
+    }
+
     //Methods conectar, tocar, desconectar, guardad
     void PlugIn() {
         System.out.println("The Bass is connect and ready to play");
--- src/oop/instruments/bass/BassMain.java
+++ src/oop/instruments/bass/BassMain.java
@@ -2,6 +2,7 @@ package oop.instruments.bass;
 
 public class BassMain {
     public static void main(String[] args) {
-
+        Bass fender = new Bass("Fender", "Green", "Precition", "Passive", 4);
+        fender.PlugIn();
     }
 }
