6a98ac8a590850b0d63a64efb5af74eb32b106ae
Carlos Munoz <carlos.munoz.us03@gmail.com>
2022-01-12 10:28:20 -0600

    Added and practice with boolean in diferent classes

--- src/oop/instruments/guitar/GuitarMain.java
+++ src/oop/instruments/guitar/GuitarMain.java
@@ -3,6 +3,11 @@ package oop.instruments.guitar;
 public class GuitarMain {
     public static void main(String[] args) {
         Guitar prs = new Guitar("PRS", "Electric Blue", "Deluxe", "Active", 6);
+        prs.TurnOn();
         prs.PlugIn();
+        prs.TurnOff();
+        prs.Unplug();
+        prs.PutAway();
+
     }
 }
