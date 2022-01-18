e61230add5a0920c2ecb38724a8ee5e475635724
Carlos Munoz <carlos.munoz.us03@gmail.com>
2022-01-18 14:49:56 -0600

    Aded family clases and practice inherence with constructors

--- src/oop/family/PersonMain.java
+++ src/oop/family/PersonMain.java
@@ -2,6 +2,19 @@ package oop.family;
 
 public class PersonMain {
     public static void main(String[] args) {
+        Person person1 = new Person("Carlos", "carlos@email.com", 33, false);
+        person1.setSleep(true);
+        person1.Sleep();
+        person1.Wakeup();
 
+        Dad papa1 = new Dad("Lucas", "lucas@email.com", 40, true);
+        papa1.Sleep();
+        papa1.setSleep(false);
+        papa1.Sleep();
+
+        Son son1 = new Son("Elias", "elias@email.com", 24, false);
+        son1.Sleep();
+        son1.setSleep(true);
+        son1.Sleep();
     }
 }
---
+++ src/oop/family/Son.java
@@ -0,0 +1,9 @@
+package oop.family;
+
+public class Son extends Person {
+    //Constructor
+
+    public Son(String name, String email, int age, boolean sleep) {
+        super(name, email, age, sleep);
+    }
+}
